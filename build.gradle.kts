eb49e7be5e70449d459ec5f803f87ab6043588db
Mirko Felice <mirko.felice@studio.unibo.it>
2022-03-18 15:48:37 +0100

    refactor: change status to state

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,12 @@ dependencies {
     implementation("com.fasterxml.jackson.core:jackson-databind:2.13.2")
 }
 
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(11))
+    }
+}
+
 tasks.compileJava {
     options.encoding = "UTF-8"
 }
@@ -20,11 +26,9 @@ tasks.compileJava {
 tasks.register<JavaExec>("scm"){
     classpath = sourceSets.main.get().runtimeClasspath
     mainClass.set("scm.SmartCoffeeMachineService")
-    args("10000", "scm")
 }
 
 tasks.register<JavaExec>("user"){
     classpath = sourceSets.main.get().runtimeClasspath
     mainClass.set("user.Application")
-    args("10000")
 }
